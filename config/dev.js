40c1c0375b7a021e9db88370e73e38390eb2d067
coolisuz <sikramov93@yahoo.com>
2020-03-25 23:52:08 +0500

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,12 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb+srv://admin:WAo1uHXqmnN6fJsT@blogpost-a2yzp.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisUrl: 'redis://127.0.0.1:6379'
 };
+
+// WAo1uHXqmnN6fJsT;
+// mongodb+srv://admin:<password>@blogpost-a2yzp.mongodb.net/test?retryWrites=true&w=majority
